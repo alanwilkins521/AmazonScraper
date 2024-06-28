a631ea5f3e5a6385bdd77a93bc1f9e90187d1d51
Alan Wilkins <103224610+alanwilkins521@users.noreply.github.com>
2024-06-28 19:22:16 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Create output csv file to write to
 File = open("out.csv", "a")
 
+# Declare headers so not seen as spam by web hosts
 HEADERS = ({'User-Agent':
            'Mozilla/5.0 (X11; Linux x86_64) 
                 AppleWebKit/537.36 (KHTML, like Gecko) 
