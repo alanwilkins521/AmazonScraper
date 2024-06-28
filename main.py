b2bd052e9ecec308e9714fad2f83a67e2759686e
Alan Wilkins <103224610+alanwilkins521@users.noreply.github.com>
2024-06-28 19:19:33 +0000

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+File = open("out.csv", "a")
+
+HEADERS = ({'User-Agent':
+           'Mozilla/5.0 (X11; Linux x86_64) 
+                AppleWebKit/537.36 (KHTML, like Gecko) 
+                    Chrome/44.0.2403.157 Safari/537.36',
+                           'Accept-Language': 'en-US, en;q=0.5'})
+
+webpage = requests.get(URL, headers=HEADERS)
+soup = BeautifulSoup(webpage.content, "lxml")
+
